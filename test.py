dece87cbe2c4fee058461ed9b7461a2ae0aa35a9
Eyüp Türkaslan <eyupcayran@gmail.com>
2023-09-15 19:37:09 +0300

    change url system

--- test.py
+++ test.py
@@ -1,67 +1,76 @@
-import json
-import requests
+import asyncio
+import aiohttp
 from tkinter import *
 from PIL import Image, ImageTk
-from tkinter.ttk import Progressbar
+from tkinter.ttk import Treeview
 
-window = Tk()
-window.title("Crypto Price")
-window.minsize(width=400, height=700)
-window.config(padx=20, pady=50)
-original_image = Image.open("crypto.png")
-resized_image = original_image.resize((200, 200))
-img = ImageTk.PhotoImage(resized_image)
+class CryptoPriceApp:
+    def __init__(self, window):
+        self.window = window
+        self.window.title("Crypto Price")
+        self.window.minsize(width=400, height=700)
+        self.window.config(padx=20, pady=20)
 
-panel = Label(window, image=img)
-panel.config(padx=200, pady=200)
-panel.pack()
+        original_image = Image.open("crypto.png")
+        resized_image = original_image.resize((200, 200))
+        self.img = ImageTk.PhotoImage(resized_image)
 
-# Defining Binance API URL
-key = "https://api.binance.com/api/v3/ticker/price?symbol="
+        self.panel = Label(self.window, image=self.img)
+        self.panel.pack(padx=20, pady=20)
 
-# Making list for multiple cryptos
-currencies = [
-    "BTCUSDT", "DOGEUSDT", "LTCUSDT", "ETHUSDT", "BNBUSDT",
-    "XRPUSDT", "ADAUSDT", "DOTUSDT", "BCHUSDT", "LINKUSDT",
-    "XLMUSDT", "TRXUSDT", "VETUSDT", "EOSUSDT", "XMRUSDT",
-    "XTZUSDT", "DASHUSDT", "ZECUSDT", "UNIUSDT", "AAVEUSDT",
-    "MKRUSDT", "COMPUSDT", "SOLUSDT", "ICPUSDT", "FTTUSDT",
-    "AVAXUSDT", "FILUSDT", "YFIUSDT", "MATICUSDT", "CAKEUSDT"
-]
+        self.treeview = Treeview(self.window, columns=("Crypto", "Price"))
+        self.treeview.heading("#1", text="Crypto")
+        self.treeview.heading("#2", text="Price")
+        self.treeview.pack(fill='both', expand=True)
 
-def get_crypto_prices():
-    name_list = []
-    j = 0
-    progress['value'] = 0
-    progress.update()
-    # Running loop to print all crypto prices
-    for i, crypto in enumerate(currencies, start=1):
-        # Completing API for request
-        url = key + crypto
-        data = requests.get(url)
-        data = data.json()
-        j = j + 1
-        name_list.append(f"{data['symbol']} price is {data['price']}")
-        my_listbox.insert(j, name_list[j - 1])
-        progress['value'] = (i / len(currencies)) * 100
-        progress.update()
+        scrollbar = Scrollbar(self.window)
+        scrollbar.pack(side='right', fill='y')
+        self.treeview.config(yscrollcommand=scrollbar.set)
+        scrollbar.config(command=self.treeview.yview)
 
-spacer2 = Label(window, text="", height=1)
-spacer2.pack()
+        spacer2 = Label(self.window, text="", height=1)
+        spacer2.pack()
 
-button = Button(text="Get Crypto Prices", command=get_crypto_prices)
-button.config()
-button.pack()
+    async def fetch_crypto_price(self, session, currency):
+        url = f"https://api.binance.com/api/v3/ticker/price?symbol={currency}"
+        async with session.get(url) as response:
+            data = await response.json()
+            price = float(data['price'])
+            formatted_price = '{:.2f}'.format(price).rstrip('0').rstrip('.')
+            return data['symbol'], formatted_price
 
-scrollbar = Scrollbar(window)
-scrollbar.pack(side='right', fill='y')
+    async def update_crypto_prices(self):
+        while True:
+            self.treeview.delete(*self.treeview.get_children())
 
-my_listbox = Listbox(window, yscrollcommand=scrollbar.set)
-my_listbox.pack(fill='both', expand=True)
+            currencies = [
+                "BTCUSDT", "DOGEUSDT", "LTCUSDT", "ETHUSDT", "BNBUSDT",
+                "XRPUSDT", "ADAUSDT", "DOTUSDT", "BCHUSDT", "LINKUSDT",
+                "XLMUSDT", "TRXUSDT", "VETUSDT", "EOSUSDT", "XMRUSDT",
+                "XTZUSDT", "DASHUSDT", "ZECUSDT", "UNIUSDT", "AAVEUSDT",
+                "MKRUSDT", "COMPUSDT", "SOLUSDT", "ICPUSDT", "FTTUSDT",
+                "AVAXUSDT", "FILUSDT", "YFIUSDT", "MATICUSDT", "CAKEUSDT"
+            ]
 
-scrollbar.config(command=my_listbox.yview)
+            async with aiohttp.ClientSession() as session:
+                tasks = [self.fetch_crypto_price(session, crypto) for crypto in currencies]
+                results = await asyncio.gather(*tasks, return_exceptions=True)
 
-progress = Progressbar(window, orient="horizontal", length=200, mode="determinate")
-progress.pack()
+                for result in results:
+                    if isinstance(result, Exception):
+                        print(result)
+                    else:
+                        symbol, price = result
+                        self.treeview.insert("", "end", values=(symbol, price))
 
-window.mainloop()
+           # await asyncio.sleep(5)
+
+def main():
+    window = Tk()
+    app = CryptoPriceApp(window)
+    loop = asyncio.get_event_loop()
+    loop.create_task(app.update_crypto_prices())
+    window.mainloop()
+
+if __name__ == "__main__":
+    main()
